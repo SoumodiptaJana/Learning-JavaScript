ba489ea3ac207891d5df94e3219dceee8ff63c77
SoumodiptaJana <soumodiptasdj@gmail.com>
2025-03-30 22:48:54 +0530

    learn to convert string to number

---
+++ stringToNumberConversion.js
@@ -0,0 +1,33 @@
+
+// let str1 = "Soumodipta"
+// let str2 = " Jana"
+
+// console.log(str1+str2);
+
+// console.log(1+"2");//12
+// console.log("1"+2);//12
+// console.log(1 + 2 + "3");//33
+// console.log("1"+2 + 3);//123
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+); // invalid
+
+
+// let number1 = 100;
+// console.log(number1++);
+// console.log(number1);
+
+let number2 = 100;
+console.log(++number2);
+console.log(number2);
+
+
+
+
+
+
+
+
+
