52a4694bcc58772d43c80c81306b0820a714879b
SoumodiptaJana <soumodiptasdj@gmail.com>
2025-03-30 22:39:44 +0530

    learn conversion of data types

---
+++ dataTypeConversion.js
@@ -0,0 +1,64 @@
+// let score = "100";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+
+// let score = "100ab";
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "100" =>100
+//"100ab" =>NaN
+//true => 1; false => 0
+
+
+
+
+// let isLogIn = 0;
+
+// console.log(typeof isLogIn);
+
+// let valueInBool = Boolean(isLogIn);
+// console.log(valueInBool);
+// console.log(typeof valueInBool);
+
+
+// let isLogIn = 1;
+
+// console.log(typeof isLogIn);
+
+// let valueInBool = Boolean(isLogIn);
+// console.log(valueInBool);
+// console.log(typeof valueInBool);
+
+
+
+// let name = "";
+
+// console.log(typeof name);
+
+// let valueInBool = Boolean(name);
+// console.log(valueInBool);
+// console.log(typeof valueInBool);
+
+
+
+let name = "Soumo";
+
+console.log(typeof name);
+
+let valueInBool = Boolean(name);
+console.log(valueInBool);
+console.log(typeof valueInBool);
+
+
+
+
