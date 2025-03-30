03a817ac82d5ffef6dc859d105932a926dec2c37
SoumodiptaJana <soumodiptasdj@gmail.com>
2025-03-30 22:26:45 +0530

    learn some basic data types

---
+++ dataTypes.js
@@ -0,0 +1,17 @@
+"use strict" //treat all JS code as newer version
+
+//Number
+let age = 26;
+
+//String
+let name = "Suman"
+
+//Boolean
+let isMale = true;
+
+console.log(typeof isMale);
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
