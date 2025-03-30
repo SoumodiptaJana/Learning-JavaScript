f2498eccd2979e9dcb5082ef5574ab22e501f8e4
SoumodiptaJana <soumodiptasdj@gmail.com>
2025-03-30 22:19:17 +0530

    learn to declare a variable

---
+++ variables.js
@@ -0,0 +1,20 @@
+const accountId = 556452;
+let accountEmail = "jana@gmain.com";
+var accountPassword = "154vdff6"; // Prefer not to use var
+accountCity = "Bangalore"; // very bad way to declare a variable
+
+// accountId = 2; //Not allowed to change const
+accountEmail = "sdj@gmain.com";
+accountPassword = "561fvd";
+accountCity = "Kolkata";
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+
+console.table({accountEmail,accountId,accountPassword});
+
+
+
