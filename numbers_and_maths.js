fdefa9671deb3d485c13a686ea56cc419cec5fef
SoumodiptaJana <soumodiptasdj@gmail.com>
2025-03-31 19:44:44 +0530

    learn math and number

---
+++ numbers_and_maths.js
@@ -0,0 +1,46 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(1000);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+// let num = 12.5683648
+// console.log(num.toPrecision(3));
+
+// const hundreds = 100000000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.1));
+
+
+// console.log(Math.max(4,8,2,6,2));
+// console.log(Math.min(4,8,2,6,1));
+
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
