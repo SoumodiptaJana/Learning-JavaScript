dea1147e419500625096cdd1502cccb9bcb1bf6b
SoumodiptaJana <soumodiptasdj@gmail.com>
2025-03-31 20:06:43 +0530

    learn date and time

---
+++ dates.js
@@ -0,0 +1,42 @@
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+
+// let createdDate = new Date(2025,0,25);//month is 0 index
+// console.log(createdDate.toDateString());
+
+// let newCreatedDate = new Date(2025,2,31,7,53,45);
+// console.log(newCreatedDate.toLocaleString());
+
+
+// let myDate = new Date("2025-01-25");
+// console.log(myDate.toDateString());
+
+// let myDate = new Date("01-22-2025");
+// console.log(myDate.toDateString());
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myTimeStamp/1000); //milisecond to second
+// console.log(Math.floor(myTimeStamp/1000)); // removing the decimals
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
+
+
+
+
+
+
+
+
